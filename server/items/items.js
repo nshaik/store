c830731f77228e452ae1443acc796739560085c2
shaik nayeem <shaik.nayeem>
2018-01-11 12:35:35 +0530

    items modified

--- server/items/items.js
+++ server/items/items.js
@@ -2,13 +2,11 @@ var express = require('express')
 var router = express.Router()
 const sqlite3 = require('sqlite3').verbose();
 
-// middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now())
   next()
 })
 
-// open database in memory
 let db = new sqlite3.Database('./database/store.db', (err) => {
   if (err) {
     return console.error(err.message);
@@ -16,19 +14,11 @@ let db = new sqlite3.Database('./database/store.db', (err) => {
   console.log('Connected to the in-memory SQlite database.');
 });
 
-// define the home page route
 router.get('/', function (req, res) {
-  /*db.each(`SELECT * FROM items`, (err, row) => {
-    if (err) {
-      console.error(err.message);
-    }
-    res.send(row);
-    console.log(row);
-  });*/
   res.send('Items');
   console.log('Items');
 })
-// define the about route
+
 router.get('/about', function (req, res) {
   res.send('About birds')
 })
@@ -70,4 +60,4 @@ db.close((err) => {
   }
   console.log('Close the database connection.');
 });
-    */
+*/
